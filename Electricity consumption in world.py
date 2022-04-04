7852e4cfb7240b775865200ba71aea228c9657f4
supercool_5 <70787564+uttam-bhai@users.noreply.github.com>
2022-04-04 07:58:30 +0530

    Add files via upload

---
+++ Electricity consumption in world.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("World_Per_Capita_Electricity_Consumption.csv")
+data.head()
---
+++ Reviewing Core Python.py
@@ -0,0 +1,6 @@
+# Reviewing Core Python
+# 1>. The basic features of the python language .
+# 2>. How to use python module mechanism .
+# 3>. How to create a new module .
+# 4>. How to create a new package .
+
